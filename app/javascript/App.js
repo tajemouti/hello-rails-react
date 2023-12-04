baa0f27c6fb6f1fe1c8586d05630f7d0653fb855
tajemouti <tajemouti@gmail.com>
2023-12-05 00:00:40 +0100

    Create a static view

---
+++ app/javascript/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const  App = () => {
+  return <>
+    <h1>Hello Rails React App!</h1>
+  </>
+}
+
+export default App;
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
