1e3427d9384bce702835dffe92209dfface9234c
tajemouti <tajemouti@gmail.com>
2023-12-05 12:52:24 +0100

    Update application.js file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,19 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 import App from "./App";
+import store from "./redux/store";
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
