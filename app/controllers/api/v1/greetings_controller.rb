a16d8088b1e50698b7e7d9349f54edf9f9a33345
tajemouti <tajemouti@gmail.com>
2023-12-05 13:37:00 +0100

    Fix linters' errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::GreetingsController < ApplicationController
   before_action :set_default_format
 
   def index
-    @greeting = Greeting.order("RANDOM()").first
+    @greeting = Greeting.order('RANDOM()').first
   end
 
   private
